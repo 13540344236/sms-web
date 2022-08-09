761b0f480782f138cfd1ce4f04f9bc25a2041fbe
“guanhai” <1297030136@qq.com>
2022-08-09 16:05:21 +0800

    供应商列表页面

--- src/views/admin/AdminsListView.vue
+++ src/views/admin/AdminsListView.vue
@@ -7,8 +7,9 @@
       <el-button style="margin-left:20px" type="primary" @click="add">添加员工品</el-button>
     </div>
 
-<!--  员工列表  -->
+
     <div>
+      <!--  员工列表  -->
       <el-table :data="tableData" border style="width: 100%">
         <el-table-column type="selection" width="55"></el-table-column>
         <el-table-column prop="id" label="员工ID" width="80"></el-table-column>
@@ -34,7 +35,7 @@
         </el-table-column>
       </el-table>
 
-<!--   添加商品   -->
+<!--   添加员工   -->
       <el-dialog title="添加员工" :visible.sync="dialogFormVisible" width="50%">
 
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px" class="demo-ruleForm">
@@ -76,29 +77,80 @@
           </el-form-item>
         </el-form>
       </el-dialog>
+
+<!--   编辑员工   -->
+      <el-dialog title="编辑员工" :visible.sync="dialogFormVisibleEdit" width="50%">
+
+        <el-form :model="editForm" :rules="rules" ref="editForm" label-width="150px" class="demo-ruleForm">
+
+          <el-form-item label="员工姓名" prop="staffName">
+            <el-input v-model="editForm.staffName"></el-input>
+          </el-form-item>
+
+          <el-form-item label="登陆密码" prop="password">
+            <el-input v-model="editForm.password"></el-input>
+          </el-form-item>
+
+          <el-form-item label="性别" prop="gender">
+            <el-input v-model="editForm.gender"></el-input>
+          </el-form-item>
+
+          <el-form-item label="电话号码" prop="phone">
+            <el-input v-model="editForm.phone"></el-input>
+          </el-form-item>
+
+          <el-form-item label="身份证号码" prop="idNumber">
+            <el-input v-model="editForm.idNumber"></el-input>
+          </el-form-item>
+
+          <el-form-item label="是否在岗" prop="onDuty">
+            <el-input v-model="editForm.onDuty"></el-input>
+          </el-form-item>
+
+          <el-form-item label="电子邮箱" prop="email">
+            <el-input v-model="editForm.email"></el-input>
+          </el-form-item>
+
+          <el-form-item label="员工简介" prop="description">
+            <el-input v-model="editForm.description"></el-input>
+          </el-form-item>
+
+          <el-form-item>
+            <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
+            <el-button type="primary" @click="handleEdit(editForm.id)">确 定</el-button>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
+    </div>
+
+
+    <!--  分页(暂未完成)  -->
+    <div style="text-align: center;margin: 20px">
+      <el-pagination background layout="total,prev, pager, next, jumper"
+                     :total="total" :page-size="pageSize"></el-pagination>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    total: {
+      type: Number,
+      default: 10
+    },
+    pageSize: {
+      type: Number,
+      default: 10
+    }
+  },
   data() {
     return {
-      tableData:[
-        {
-          id:1,
-          staffName: '张三',
-          password: '123465',
-          gender: '男',
-          phone: '12344448888',
-          idNumber: '5148486848181181851',
-          onDuty: '在岗',
-          email: 'zhangsan@qq.com',
-          description: '平时偷奸耍滑,建议开除'
-        }
-      ],
+      tableData:[{id:'1', staffName: '张三', password: '123456', gender: '男', phone:'13255584449',
+        idNumber: '514845151515851', onDuty: '1', email:'zhangsan@qq.com', description:'上班偷奸耍滑,建议开除'}],
       input:'',
       dialogFormVisible:false,
+      dialogFormVisibleEdit:false,
       ruleForm: {
         id:'', staffName: '', password: '', gender: '', phone:'',
         idNumber: '', onDuty: '', email:'', description:''
@@ -132,6 +184,10 @@ export default {
     }
   },
   methods: {
+// 员工列表
+    loadGoods: function () {
+      console.log('loadGoods()');
+    },
 // 添加员工
     add(){
       this.dialogFormVisible = true
@@ -140,7 +196,39 @@ export default {
 
     },
     resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
 
+// 删除员工
+    openDeleteConfirm(id) {
+      this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.handleDelete(id);
+      }).catch(() => {
+      });
+    },
+    handleDelete(id){
+
+    },
+
+// 编辑员工
+    edit(val) {
+      Object.assign(this.editForm,val)
+      this.dialogFormVisibleEdit = true;
+    },
+    handleEdit(id) {
+
+    },
+
+    created() {
+      console.log('vue created')
+    },
+    mounted() {
+      console.log('vue mounted')
+      this.loadGoods();
     }
   }
 }
--- src/views/supplier/SupplierListView.vue
+++ src/views/supplier/SupplierListView.vue
@@ -1,13 +1,55 @@
 <template>
-<h1>这是供货商管理</h1>
+  <div>
+    <h3 style="margin: 20px 0">供货商管理</h3>
+    <div style="display: flex;margin: 20px 0">
+      <el-input style="width: 180px" v-model="input" placeholder="请输入内容"></el-input>
+      <el-button style="margin-left:20px" type="primary">搜索</el-button>
+      <el-button style="margin-left:20px" type="primary" @click="add">添加供货商</el-button>
+    </div>
+
+    <div>
+<!--   供货商列表   -->
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="id" label="供应商ID" width="80"></el-table-column>
+        <el-table-column prop="supplier" label="供应商" width="150"></el-table-column>
+        <el-table-column prop="supplierName" label="供应商姓名" width="140"></el-table-column>
+        <el-table-column prop="supplierPhone" label="供应商电话" width="140"></el-table-column>
+        <el-table-column prop="supplierSite" label="供应商地址"></el-table-column>
+        <el-table-column prop="remark" label="备注"></el-table-column>
+        <el-table-column label="操作" width="180">
+          <template slot-scope="scope">
+            <el-button
+                size="mini"
+                type="primary"
+                @click="edit(scope.row)">编辑</el-button>
+            <el-button
+                size="mini"
+                type="danger"
+                @click="openDeleteConfirm(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "SupperlierListView"
+  data() {
+    return {
+      tableData:[
+          {id:'1', supplier: '张兵调料批发', supplierName: '张兵',
+            supplierPhone: '13344445555', supplierSite:'飞燕路199号', remark: '张三的叔叔'}
+      ],
+      input:'',
+      ruleForm: {
+        id:'', supplier: '', supplierName: '', supplierPhone: '', supplierSite:'', remark: ''
+      },
+      editForm: {
+        id:'', supplier: '', supplierName: '', supplierPhone: '', supplierSite:'', remark: ''
+      },
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
