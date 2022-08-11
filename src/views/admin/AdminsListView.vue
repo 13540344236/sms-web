2b02de5e8a8b1624b91f1aba7468581e8e3d90df
86187 <3203790247@qq.com>
2022-08-11 16:49:36 +0800

    员工前端页面

--- src/views/admin/AdminsListView.vue
+++ src/views/admin/AdminsListView.vue
@@ -3,24 +3,28 @@
     <h3 style="margin: 20px 0">员工管理</h3>
     <div style="display: flex;margin: 20px 0">
       <el-input style="width: 180px" v-model="input" placeholder="请输入内容"></el-input>
-      <el-button style="margin-left:20px" type="primary">搜索</el-button>
+      <el-button style="margin-left:20px" type="primary" @click="selects()">搜索</el-button>
       <el-button style="margin-left:20px" type="primary" @click="add">添加员工</el-button>
+      <!--      <el-button style="margin-left:20px" type="primary" @click="addNew">添加商品</el-button>-->
     </div>
 
-
     <div>
-      <!--  员工列表  -->
-      <el-table :data="tableData" border style="width: 100%">
+      <!--            <el-breadcrumb separator-class="el-icon-arrow-right">-->
+      <!--              <el-breadcrumb-item :to="{ path: '/sms/goods/list' }">首页</el-breadcrumb-item>-->
+      <!--              <el-breadcrumb-item>商品管理</el-breadcrumb-item>-->
+      <!--            </el-breadcrumb>-->
+
+      <el-table :data="tableData" border style="width: 100%;text-align: center">
         <el-table-column type="selection" width="55"></el-table-column>
-        <el-table-column prop="id" label="员工ID" width="80"></el-table-column>
-        <el-table-column prop="staffName" label="员工姓名" width="100"></el-table-column>
-        <el-table-column prop="password" label="登陆密码" width="140"></el-table-column>
-        <el-table-column prop="gender" label="性别" width="80"></el-table-column>
-        <el-table-column prop="phone" label="电话号码" width="140"></el-table-column>
-        <el-table-column prop="idNumber" label="身份证号码"></el-table-column>
-        <el-table-column prop="onDuty" label="是否在岗" width="120"></el-table-column>
-        <el-table-column prop="email" label="电子邮箱"></el-table-column>
-        <el-table-column prop="description" label="员工简介"></el-table-column>
+        <el-table-column prop="id" label="员工ID" width="100"></el-table-column>
+        <el-table-column prop="staffName" label="员工名称" width="140"></el-table-column>
+        <el-table-column prop="gender" label="员工性别" width="80"></el-table-column>
+        <el-table-column prop="phone" label="员工电话号码" width="140"></el-table-column>
+        <el-table-column prop="idNumber" label="身份证号" width="140"></el-table-column>
+        <el-table-column prop="onDuty" label="是否在岗" width="140"></el-table-column>
+        <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
+        <el-table-column prop="description" label="描述" width="140"></el-table-column>
+        <el-table-column prop="enable" label="是否启用" width="140"></el-table-column>
         <el-table-column label="操作" width="150">
           <template slot-scope="scope">
             <el-button
@@ -35,7 +39,7 @@
         </el-table-column>
       </el-table>
 
-<!--   添加员工   -->
+      <!--  添加商品  -->
       <el-dialog title="添加员工" :visible.sync="dialogFormVisible" width="50%">
 
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px" class="demo-ruleForm">
@@ -43,19 +47,11 @@
             <el-input v-model="ruleForm.staffName"></el-input>
           </el-form-item>
 
-          <el-form-item label="登陆密码" prop="password">
-            <el-input v-model="ruleForm.password"></el-input>
-          </el-form-item>
-
-          <el-form-item label="性别" prop="gender">
+          <el-form-item label="员工性别" prop="gender">
             <el-input v-model="ruleForm.gender"></el-input>
           </el-form-item>
 
-          <el-form-item label="电话号码" prop="phone">
-            <el-input v-model="ruleForm.phone"></el-input>
-          </el-form-item>
-
-          <el-form-item label="身份证号码" prop="idNumber">
+          <el-form-item label="身份证号" prop="idNumber">
             <el-input v-model="ruleForm.idNumber"></el-input>
           </el-form-item>
 
@@ -63,13 +59,14 @@
             <el-input v-model="ruleForm.onDuty"></el-input>
           </el-form-item>
 
-          <el-form-item label="电子邮箱" prop="email">
+          <el-form-item label="邮箱" prop="email">
             <el-input v-model="ruleForm.email"></el-input>
           </el-form-item>
 
-          <el-form-item label="员工简介" prop="description">
+          <el-form-item label="描述" prop="description">
             <el-input v-model="ruleForm.description"></el-input>
           </el-form-item>
+
 
           <el-form-item>
             <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
@@ -78,53 +75,47 @@
         </el-form>
       </el-dialog>
 
-<!--   编辑员工   -->
+      <!--   编辑商品   -->
       <el-dialog title="编辑员工" :visible.sync="dialogFormVisibleEdit" width="50%">
 
-        <el-form :model="editForm" :rules="rules" ref="editForm" label-width="150px" class="demo-ruleForm">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="150px" class="demo-ruleForm">
 
-          <el-form-item label="员工姓名" prop="staffName">
-            <el-input v-model="editForm.staffName"></el-input>
+          <el-form-item label="员工ID" prop="id">
+            <el-input v-model="ruleForm.id" :disabled="true"></el-input>
           </el-form-item>
 
-          <el-form-item label="登陆密码" prop="password">
-            <el-input v-model="editForm.password"></el-input>
+          <el-form-item label="员工名称" prop="staffName">
+            <el-input v-model="ruleForm.staffName"></el-input>
           </el-form-item>
 
-          <el-form-item label="性别" prop="gender">
-            <el-input v-model="editForm.gender"></el-input>
+          <el-form-item label="员工性别" prop="gender">
+            <el-input v-model="ruleForm.gender"></el-input>
           </el-form-item>
 
-          <el-form-item label="电话号码" prop="phone">
-            <el-input v-model="editForm.phone"></el-input>
+          <el-form-item label="员工电话号码" prop="phone">
+            <el-input v-model="ruleForm.phone"></el-input>
           </el-form-item>
 
-          <el-form-item label="身份证号码" prop="idNumber">
-            <el-input v-model="editForm.idNumber"></el-input>
+          <el-form-item label="身份证号" prop="idNumber">
+            <el-input v-model="ruleForm.idNumber"></el-input>
           </el-form-item>
 
-          <el-form-item label="是否在岗" prop="onDuty">
-            <el-input v-model="editForm.onDuty"></el-input>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="ruleForm.email"></el-input>
           </el-form-item>
 
-          <el-form-item label="电子邮箱" prop="email">
-            <el-input v-model="editForm.email"></el-input>
+          <el-form-item label="简介" prop="description">
+            <el-input v-model="ruleForm.description"></el-input>
           </el-form-item>
-
-          <el-form-item label="员工简介" prop="description">
-            <el-input v-model="editForm.description"></el-input>
-          </el-form-item>
-
           <el-form-item>
             <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
-            <el-button type="primary" @click="handleEdit(editForm.id)">确 定</el-button>
+            <el-button type="primary" @click="handleEdit(ruleForm.id)">确 定</el-button>
           </el-form-item>
         </el-form>
       </el-dialog>
     </div>
 
-
-    <!--  分页(暂未完成)  -->
+    <!--  分页  -->
     <div style="text-align: center;margin: 20px">
       <el-pagination background layout="total,prev, pager, next, jumper"
                      :total="total" :page-size="pageSize"></el-pagination>
@@ -146,62 +137,59 @@ export default {
   },
   data() {
     return {
-      tableData:[{id:'1', staffName: '张三', password: '123456', gender: '男', phone:'13255584449',
-        idNumber: '514845151515851', onDuty: '1', email:'zhangsan@qq.com', description:'上班偷奸耍滑,建议开除'}],
+      tableData: [],
       input:'',
       dialogFormVisible:false,
       dialogFormVisibleEdit:false,
       ruleForm: {
-        id:'', staffName: '', password: '', gender: '', phone:'',
-        idNumber: '', onDuty: '', email:'', description:''
-      },
-      editForm: {
-        id:'', staffName: '', password: '', gender: '', phone:'',
-        idNumber: '', onDuty: '', email:'', description:''
+        id:'',
+        staffName: '',
+        password: '',
+        gender: '',
+        phone:'',
+        idNumber: '',
+        onDuty: '',
+        email:'',
+        description:'',
+        enable:'',
+        loginCount:'',
+        gmtLastLogin:'',
+        gmtCreate:'',
+        gmtModified:''
+
       },
       rules: {
-        staffName: [
-          { required: true, message: '请输入员工姓名', trigger: 'blur' },
-          { min: 2, max: 15, message: '长度在 2 到 15 个字符', trigger: 'blur' }
+        supplierPhone: [
+          { required: true, message: '销售价格', trigger: 'blur' },
+          { pattern:/^1[3456789]\d{9}$/g, message: '请输入数字', trigger: 'blur' }
         ],
-        password: [
-          { required: true, message: '请输入员工登录密码', trigger: 'blur' },
-          { min: 2, max: 15, message: '长度在 2 到 15 个字符', trigger: 'blur'}
+        purchasePrice: [
+          { required: true, message: '销售价格', trigger: 'blur' },
+          { pattern:/([1-9]\d*\.?\d*)|(0\.\d*[1-9])/, message: '请输入数字', trigger: 'blur' }
         ],
-        email: [
-          { required: true, message: '请输入员工邮箱', trigger: 'blur' },
-          { pattern:/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, message: '请输入正确的邮箱', trigger: 'blur' }
-        ],
-        phone: [
-          { required: true, message: '请输入员工电话号码', trigger: 'blur' },
-          { pattern:/^1[3|4|5|7|8][0-9]\d{4,8}$/, message: '请输入正确的手机号码', trigger: 'blur' }
-        ],
-        idNumber: [
-          { required: true,message: '请输入员工身份证号码', trigger: 'blur' },
-          { pattern:/(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/, min: 1, max: 14, message: '请输入正确的身份证号码', trigger: 'blur' }
+        goodsSpecification: [
+          { message: '商品规格', trigger: 'blur' },
+          { min: 1, max: 50, message: '长度在 3 到 50 个字符', trigger: 'blur' }
         ]
       }
     }
   },
   methods: {
-// 员工列表
     loadGoods: function () {
       console.log('loadGoods()');
+      let url = 'http://localhost:9091/admins';
+      console.log('url = ' + url);
+      this.axios.get(url).then((response) => {
+        let json = response.data;
+        if (json.code === 20000) {
+          this.tableData = json.data;
+        } else {
+          this.$message.error(response.data.message);
+        }
+      })
     },
-// 添加员工
-    add(){
-      this.dialogFormVisible = true
-    },
-    submitForm(formName) {
-
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    },
-
-// 删除员工
     openDeleteConfirm(id) {
-      this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+      this.$confirm('此操作将永久删除该员工, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
@@ -210,26 +198,105 @@ export default {
       }).catch(() => {
       });
     },
-    handleDelete(id){
-
+    handleDelete(id) {
+      console.log('将删除id = ' + id + '的员工数据');
+      let url = 'http://localhost:9091/admins/' + id + '/delete'
+      this.axios.post(url).then((response) => {
+        let json = response.data;
+        if (json.code === 20000) {
+          this.$message({
+            message: '删除员工成功！',
+            type: 'success'
+          })
+        } else {
+          this.$message.error(response.data.message);
+        }
+        this.loadGoods();
+      });
+    },
+//查询管理员
+    selects(id){
+      console.log('将查询id = ' + id + '的员工数据');
+      let url = 'http://localhost:9091/admins/' + id + '/selectById'
+      this.axios.get(url).then((response) => {
+        let json = response.data;
+        if (json.code === 20000) {
+          this.tableData=json;
+          this.$message({
+            message: '查询员工成功！',
+            type: 'success'
+          })
+        } else {
+          this.$message.error(response.data.message);
+        }
+        this.loadGoods();
+      });
+    },
+// 添加管理员
+    add(){
+      this.dialogFormVisible = true
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // alert('submit!');
+          let url = 'http://localhost:9091/admins/add-new';
+          console.log('url >>>'+url);
+          console.log('data >>>');
+          console.log("参数:" + this.ruleForm);
+          this.axios.post(url,this.ruleForm).then((response) => {
+            console.log(response.data);
+            if (response.data.code === 20000){
+              this.$message({
+                message: '添加员工成功！',
+                type: 'success'
+              });
+              this.dialogFormVisible = false;
+              this.loadGoods();
+            }else {
+              this.$message.error(response.data.message);
+            }
+          }).catch(function (error) {
+            console.log('响应结果失败!')
+          })
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     },
 
-// 编辑员工
-    edit(val) {
-      Object.assign(this.editForm,val)
+// 编辑商品
+    edit(val){
+      Object.assign(this.ruleForm,val)
       this.dialogFormVisibleEdit = true;
     },
     handleEdit(id) {
-
-    },
-
-    created() {
-      console.log('vue created')
-    },
-    mounted() {
-      console.log('vue mounted')
-      this.loadGoods();
+      console.log('将编辑id = ' + id + '的员工数据');
+      let url = 'http://localhost:9091/admins/update'
+      this.axios.post(url,this.ruleForm).then((response) => {
+        let json = response.data;
+        console.log(response.data.data)
+        if (json.code === 20000) {
+          this.$message({
+            message: '编辑员工成功！',
+            type: 'success'
+          });
+          this.dialogFormVisibleEdit = false;
+          this.loadGoods();
+        } else {
+          this.$message.error(response.data.message);
+        }
+        this.loadGoods();
+      });
     }
+  },
+  mounted() {
+    console.log('vue mounted')
+    this.loadGoods();
   }
 }
 </script>
